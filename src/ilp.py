b655236ded0c7d51f4a3bb3d9be7c34e61dbfe84
tevn <tevn@textio.com>
2019-06-08 17:44:17 -0700

    remove ilp module

--- src/ilp.py
+++
@@ -1,8 +0,0 @@
-import operator
-
-def run_ilp_program(probability_distributions):
-    labels = []
-    for token_distribution in probability_distributions:
-        label = max(token_distribution.items(), key=operator.itemgetter(1))[0]
-        labels.append(label)
-    return labels
