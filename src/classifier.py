88461404b75e9df48a5f7b585ab5ebcc08ed720c
hayley luke <jhluke@uw.edu>
2019-06-08 19:34:34 -0700

    preliminary ilp

--- src/classifier.py
+++ src/classifier.py
@@ -1,4 +1,5 @@
 import operator
+import pulp
 
 def word2features(sent, i):
     # Best features from paper (tokens + POS tags):
@@ -67,9 +68,93 @@ def sent2labels(sent):
 
 def optimize(probability_distributions):
     labels = []
-    for token_distribution in probability_distributions:
-        label = max(token_distribution.items(), key=operator.itemgetter(1))[0]
+    # for token_distribution in probability_distributions:
+    #     label = max(token_distribution.items(), key=operator.itemgetter(1))[0]
+    #     labels.append(label)
+    # return labels
+
+    lp = pulp.LpProblem("Classify", pulp.LpMaximize)
+
+    # Collapse the entire sentence into a single dictionary
+    # Append each tag with _i where i is its index in the sentence
+
+    # Generate the tag name based on index
+    tag_name = lambda t, i: '{}_{}'.format(t, i)
+
+    probs = {}
+    for i, tags in enumerate(probability_distributions):
+        for tag, prob in tags.items():
+            probs[tag_name(tag, i)] = prob
+    sent_length = len(probability_distributions)
+    num_tags = len(probs)
+
+    lp_vars = pulp.LpVariable.dicts("tags", probs, lowBound=0, cat="Integer")
+
+    # Get a variable from lp_vars based on tag name and index
+    get_var = lambda t, i: lp_vars[tag_name(t, i)]
+
+    # Set the objective function. maximixe the sum of all probabilities across the sentence
+    lp += pulp.lpSum([probs[tag] * lp_vars[tag] for tag in probs.keys()])
+
+    # Set constaints for each token in the sentence
+    for i in range(sent_length):
+        be = get_var('BE', i)
+        be_ip = get_var('BE-IP', i)
+        ip = get_var('IP', i)
+        ie = get_var('IE', i)
+        o = get_var('O', i)
+
+        # There can only be one tag for every token
+        lp += pulp.lpSum([be, be_ip, ip, ie, o]) == 1
+
+        if i == 0:
+            # First tag can only be a BE, BE-IP, or O
+            lp += pulp.lpSum([be, be_ip, o]) == 1
+
+        if i == sent_length - 1:
+            # Last tag can only be a BE-IP, IP, or O
+            lp += pulp.lpSum([be_ip, ip, o]) == 1
+
+        if i > 0:
+            prev_be = get_var('BE', i - 1)
+            prev_be_ip = get_var('BE-IP', i - 1)
+            prev_ip = get_var('IP', i - 1)
+            prev_ie = get_var('IE', i - 1)
+            prev_o = get_var('O', i - 1)
+
+            # BE-IP|IP|O -> BE
+            lp += pulp.lpSum([be, -1 * prev_be_ip, -1 * prev_ip, -1 * prev_o]) <= 0
+
+            # BE-IP|IP|O -> BE-IP
+            lp += pulp.lpSum([be_ip, -1 * prev_be_ip, -1 * prev_ip, -1 * prev_o]) <= 0
+
+            # BE|BE-IP|IP|IE -> IP
+            lp += pulp.lpSum([ip, -1 * prev_be, -1 * prev_be_ip, -1 * prev_ip, -1 * prev_ie]) <= 0
+
+            # BE|BE-IP|IP|IE -> IE
+            lp += pulp.lpSum([ie, -1 * prev_be, -1 * prev_be_ip, -1 * prev_ip, -1 * prev_ie]) <= 0
+
+            # BE-IP|IP|O -> O
+            lp += pulp.lpSum([o, -1 * prev_be_ip, -1 * prev_ip, -1 * prev_o]) <= 0
+
+            # Can't go BE to BE
+            lp += pulp.lpSum([-1 * be, -1 * prev_be, -1 * prev_ip, -1 * prev_o]) >= -1
+
+    lp.solve()
+
+    # Now we gotta rebuild the tag sequence from the results
+    for i in range(sent_length):
+        dist = {
+            'BE': get_var('BE', i).varValue,
+            'BE-IP': get_var('BE-IP', i).varValue,
+            'IP': get_var('IP', i).varValue,
+            'IE': get_var('IE', i).varValue,
+            'O': get_var('O', i).varValue,
+        }
+
+        label = max(dist.items(), key=operator.itemgetter(1))[0]
         labels.append(label)
+
     return labels
 
 def run_ilp_program(predictions, flatten=True):
